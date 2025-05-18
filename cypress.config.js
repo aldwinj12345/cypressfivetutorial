434952f57afa4338d98fc3a569c8e8867097a67d
Aldwin J <aldwinj12345@gmail.com>
2025-05-18 16:56:40 +0800

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require('cypress')
+const fs = require('fs')
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
+  video : true,
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      on('after:spec', (spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) =>
+            test.attempts.some((attempt) => attempt.state === 'failed')
+          )
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video)
+          }
+        }
+      })
+    },
+  },
+})
