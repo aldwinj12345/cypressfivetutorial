87a0e5a8e7f8d4b9e505065d802922c7f7e04e18
Aldwin J <aldwinj12345@gmail.com>
2025-05-18 20:50:03 +0800

    hjghj

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: 'mochawesome',
+  reporterOptions: {
+    overwrite: false,
+    html: false,
+    json: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       //require('cypress-mochawesome-reporter/plugin')(on);
